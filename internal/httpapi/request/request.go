8aa5cc5bf327ac485370829bf5c09751d874f365
mkhamat <streetmc1337@gmail.com>
2026-08-29 13:24:39 +0300

    chore: хелпер

--- internal/httpapi/request/request.go
+++ internal/httpapi/request/request.go
@@ -1,13 +1,24 @@
 package request
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"nilchan-hackaton/internal/httpapi/response"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/go-playground/validator/v10"
 )
+
+func PathInt64(r *http.Request, name string) (int64, error) {
+	value, err := strconv.ParseInt(chi.URLParam(r, name), 10, 64)
+	if err != nil || value <= 0 {
+		return 0, fmt.Errorf("invalid %s", name)
+	}
+	return value, nil
+}
 
 func DecodeAndValidate[T any](w http.ResponseWriter, r *http.Request, validate *validator.Validate) (*T, bool) {
 	var request T
